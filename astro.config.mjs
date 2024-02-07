3a665606c43245e15cc9a5efc845244b47b6aa80
Alex Karpekov <akarpekov3@gatech.edu>
2024-02-06 23:13:49 -0500

    edit sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://www.alexkarpekov.com',
   integrations: [mdx(), sitemap(), tailwind()],
   // output: 'server',
   // adapter: vercel({
